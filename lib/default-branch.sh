284b7a97447a239a46138d132604b13bbcb43f54
Brendan McMillion <brendan@cloudflare.com>
2020-07-12 21:53:55 -0500

    test6

--- lib/default-branch.sh
+++ lib/default-branch.sh
@@ -2,8 +2,8 @@
 # Usage: $0 [remote]
 # Get the name of the remote HEAD for the named remote (default: origin).
 
-echo "BRENDAN-${GIT_REMOTE}"
 origin="${1:-${GIT_REMOTE:-origin}}"
+echo "BRENDAN-${GIT_REMOTE}-$origin"
 branch=$(git rev-parse --abbrev-ref "$origin/HEAD" 2>/dev/null)
 if [[ "$branch" == "$origin/HEAD" ]]; then
     echo "ASDF"
