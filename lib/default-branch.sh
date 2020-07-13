1741f53e1afcd92d8cb84cc3d8f195adad2a8f75
Brendan McMillion <brendan@cloudflare.com>
2020-07-12 21:55:52 -0500

    test7

--- lib/default-branch.sh
+++ lib/default-branch.sh
@@ -10,7 +10,7 @@ if [[ "$branch" == "$origin/HEAD" ]]; then
     # If $origin doesn't exist, then the above rev-parse will fail, but that is
     # OK.  We can let git remote report the error.
     set -e
-    git remote set-head -a "$origin" 2>/dev/null 1>&2
+    git remote set-head -a "$origin" 2>&1 # 2>/dev/null 1>&2
     branch=$(git rev-parse --abbrev-ref "$origin/HEAD" 2>/dev/null)
     echo "MMMM"
 fi
